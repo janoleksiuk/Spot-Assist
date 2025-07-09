3fabf2238e2a1b5c81966fe8e1d4559b7dfb384a
unknown <j.oleksiuk_ladm@PIAPN003272.lukasiewicz.local>
2025-07-09 16:08:16 +0200

    added initial (2 seqs hadled with primitive robot beahviours) pose_control_v2 script with action_code node in prod

--- Main/detect_human_action.py
+++ Main/detect_human_action.py
@@ -1,11 +1,14 @@
-POSE_ENDPOINT_PATH = r'C:\Users\j.oleksiuk_ladm\Desktop\Spot Ecosystem\prod\behaviour_code.txt'
+import time
+
+POSE_ENTRYPOINT_PATH = r'C:\Users\j.oleksiuk_ladm\Desktop\Spot Ecosystem\prod\behaviour_code.txt'
+POSE_ENDPOINT_PATH = r'C:\Users\j.oleksiuk_ladm\Desktop\Spot Ecosystem\prod\action_code.txt'
 
 #function retrieving detected pose code from endpoint (.txt file)
 def get_pose():
 
     while(True):
         try:
-            with open(POSE_ENDPOINT_PATH, 'r') as f:
+            with open(POSE_ENTRYPOINT_PATH, 'r') as f:
                 endpoint_content = f.read().strip()  
                 if endpoint_content == '':
                     continue
@@ -20,32 +23,46 @@ def handle_sequence(seq):
     
     # sitting - standing - sitting
     if seq[-3:] == '010':
-        print("SEQ 010 SPOTTED")
+        try:
+            with open(POSE_ENDPOINT_PATH, 'w') as f:
+                f.write('1')
+        except Exception as e:
+            print(e)
+            return False
+
         return True
     
-    else:
-        return False
-    
+    # standing - standing 1hand - standing - standing 1hand
+    if seq[-4:] == '1313':
+        try:
+            with open(POSE_ENDPOINT_PATH, 'w') as f:
+                f.write('2')
+        except Exception as e:
+            print(e)
+            return False
 
+        return True
+    
+    return False
+    
 def main():
 
-    sequence = ''
+    sequence = 'a' # starting seq cant be null due to sequence length
     prev_pose = None
     sequence_handled = False
 
     while(True):
         pose = get_pose()
-        print(sequence)
 
         if pose != prev_pose:
             sequence += str(pose)
             prev_pose = pose
         
-        if len(sequence) > 2:
+        if len(sequence) > 3:
             sequence_handled = handle_sequence(sequence)
 
         if sequence_handled:
-            sequence = ''
+            sequence = 'a'
             prev_pose = None
             sequence_handled = False
 
--- prod/README.txt
+++ prod/README.txt
@@ -1,3 +1,6 @@
-this is production directory - do not work here
+this is folder for files used by system
+
+behaviour_code -> used in GESTURE CONTROL
+action_code -> used in POSE (ACTION) CONTROL
 
 C:\Users\j.oleksiuk_ladm\Desktop\Spot Ecosystem\prod
